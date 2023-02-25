e038e4ddbf6d9dc559a2c09f648724bcb2d3cf01
Felix Breitenbach <felix.breitenbach@gmail.com>
2023-02-25 20:05:39 +0100

    exercise 3

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { range } from "../../utils.js";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
+
+import Guess from "../Guess";
 
 function GuessResults({ guesses }) {
+  const rows = [
+    ...guesses,
+    ...range(NUM_OF_GUESSES_ALLOWED).map((_) => ""),
+  ].slice(0, 6);
+
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p key={index} className="guess">
-          {guess}
-        </p>
+      {rows.map((guess, index) => (
+        <Guess key={index} guess={guess} />
       ))}
     </div>
   );
